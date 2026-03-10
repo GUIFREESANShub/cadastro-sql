233c3d072cca16c58d251ab2aeb3c14c3259d4f2
GUIFREESANShub <guilherme.frigeri.santos@escola.pr.gov.br>
2026-03-10 14:56:40 +0000

    gui2

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,36 @@
+const express = require ('express');
+const mysql = require ('mysql');
+const cors = require ('cors');
+const { error } = require('node:console');
 
+const app = express ();
+
+app.use (cors());
+app.use (express.json());
+
+ // criar conexão com o banco
+const conexao = mysql.createconetion(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'escola',
+
+    }
+) ;
+ //criar nota para salvar aluno 
+  app.post ("/salvar",(req, res)=>{
+  let nome = req.body.nome
+  let idade = req.body.idade;
+  let sql = "INSERT INTO alunos (nome, idade ) VALUES (?, ?)";
+
+  conexao. query (sql, [nome, idade],)( erro,resultado => {
+    if (erro) {
+        console.log(erro);
+    }else{
+        res.send("Aluno salvo com sucesso")
+     }
+    });
+});
+  
+  
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function Salvar() {
     let nome = document.getElementById('Nome').value
     let idade = document.getElementById('Idade').value
-    console.log('Nome:',nome)
-    console.log('Idade:',idade)
+    console.log('nome:',nome)
+    console.log('idade:',idade)
 }
